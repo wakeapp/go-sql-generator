2314aadf8bef4001b18219ae24bf1026a820c2e2
Denis <dlimarev@wakeapp.ru>
2021-02-10 13:21:40 +0300

    HUB-398

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/wakeapp/go-sql-generator
+
+go 1.15
+
+require github.com/jmoiron/sqlx v1.3.1
---
+++ sg_test.go
@@ -0,0 +1,102 @@
+package sg
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestGetInsertSQL(t *testing.T) {
+	tests := []struct {
+		name        string
+		fields      []string
+		valuesStack [][]string
+		expected    map[string]string
+	}{
+		{
+			fields: []string{
+				"field_1",
+				"field_2",
+			},
+			valuesStack: [][]string{
+				{
+					"field_1",
+					"field_2",
+				},
+				{
+					"field_1_1",
+					"field_2_1",
+				},
+			},
+		},
+		{
+			fields: []string{
+				"field_1",
+				"field_2",
+			},
+			valuesStack: [][]string{
+				{
+					"field_1_2",
+					"field_2_2",
+				},
+				{
+					"field_1_3",
+					"field_2_3",
+				},
+				{
+					"field_1_4",
+					"field_2_4",
+				},
+				{
+					"field_1_5",
+					"field_2_5",
+				},
+			},
+		},
+		{
+			fields: []string{
+				"field_1",
+				"field_2",
+			},
+			valuesStack: [][]string{
+				{
+					"field_1_2",
+					"field_2_2",
+				},
+			},
+		},
+	}
+
+	var sqlGenerator = MysqlSqlGenerator{}
+	var dataInsert InsertData
+	var valuesCount int
+
+	for _, test := range tests {
+		valuesCount = 0
+		dataInsert = InsertData{
+			TableName: "TestTable",
+			Fields:    test.fields,
+		}
+
+		for _, values := range test.valuesStack {
+			dataInsert.Add(values)
+			valuesCount += len(values)
+		}
+
+		query, args, err := sqlGenerator.GetInsertSql(dataInsert)
+		if err != nil {
+			t.Fatalf("on GetInsertSql: %s", err)
+		}
+
+		if actualValuesCount := strings.Count(query, "?"); actualValuesCount != valuesCount {
+			t.Fatalf("on compare values count and params: expected: %d, actual: %d", valuesCount, actualValuesCount)
+		}
+
+		if actualValuesCount := strings.Count(query, "?"); actualValuesCount != len(args) {
+			t.Fatalf("on compare values count and args: expected: %d, actual: %d", valuesCount, len(args))
+		}
+
+		if valuesCount != len(args) {
+			t.Fatalf("on compare values count and args: expected: %d, actual: %d", valuesCount, len(args))
+		}
+	}
+}
